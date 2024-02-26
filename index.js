6b4324b78756c99d860ffb4158329927f0c6f1d3
Colorful Invisible <realismofantastico@gmail.com>
2024-02-27 00:37:00 +0100

    remove comments

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 const panels = document.querySelectorAll(".panel");
 
-// Create a new timeline with ScrollTrigger
 const tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".wrapper",
@@ -13,9 +12,7 @@ const tl = gsap.timeline({
   },
 });
 
-// Dynamically add animation steps for each panel
 panels.forEach((panel, i) => {
-  // For the first panel, scale it down immediately
   if (i === 0) {
     tl.to(panel, { transform: "scaleY(0)", ease: "none" });
   } else if (i === panels.length - 1) {
